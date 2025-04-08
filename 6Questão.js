27e2b84ca0c739969122d72d6fdf27d90a2a4fe2
marciliooff <marcilioav46@gmail.com>
2025-04-08 18:43:50 +0000

    exercício 6 finalizado

---
+++ 6Questão.js
@@ -0,0 +1,13 @@
+function verificarnumero(n1){
+    if (n1 > 0){
+        console.log("Positivo")
+    } else if (n1 < 0){
+        console.log ("negativo")
+    } else 
+    console.log ("Zero")
+} 
+
+
+verificarnumero(2)
+verificarnumero(0)
+verificarnumero(-1)
